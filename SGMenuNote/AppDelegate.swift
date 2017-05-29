282ca16ab3a4c72abd4a90a97acdce9a8ea4c59e
이세건 <segunleedev@gmail.com>
2017-05-29 17:15:31 +0900

    Initial Commit

---
+++ SGMenuNote/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SGMenuNote
+//
+//  Created by SegunLee on 2017. 5. 29..
+//  Copyright © 2017년 SGIOS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SGMenuNote/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SGMenuNote
+//
+//  Created by SegunLee on 2017. 5. 29..
+//  Copyright © 2017년 SGIOS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
